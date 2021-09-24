49357d19a2e37aa2cf68070962798b9edd569442
Alessandro Pangrazio <alessandropangrazio@live.com>
2021-09-24 15:26:37 +0930

    addLstmap

---
+++ ft_lstmap.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwander <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/20 13:51:37 by jwander           #+#    #+#             */
+/*   Updated: 2021/09/20 14:17:17 by jwander          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_lst;
+	t_list	*temp;
+
+	if (!lst || !f)
+		return (NULL);
+	new_lst = 0;
+	while (lst)
+	{
+		temp = ft_lstnew((*f)(lst->content));
+		if (!temp)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, temp);
+		temp = temp->next;
+		lst = lst->next;
+	}
+	return (new_lst);
+}
